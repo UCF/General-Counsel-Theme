5539ac2c5f24185be1d38376b39ba6703f0cf151
Brandon Groves <brandon.groves@gmail.com>
2013-10-14 14:12:24 -0400

    updated university header

--- functions/config.php
+++ functions/config.php
@@ -297,7 +297,7 @@ Config::$styles = array(
 
 Config::$scripts = array(
 	array('admin' => True, 'src' => THEME_JS_URL.'/admin.js',),
-	'http://universityheader.ucf.edu/bar/js/university-header.js',
+	'//universityheader.ucf.edu/bar/js/university-header.js?use-bootstrap-overrides=1',
 	THEME_STATIC_URL.'/bootstrap/bootstrap/js/bootstrap.js',
 	array('name' => 'base-script',  'src' => THEME_JS_URL.'/webcom-base.js',),
 	array('name' => 'theme-script', 'src' => THEME_JS_URL.'/script.js',),
